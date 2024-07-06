6bea34848f97ccc1b7a9dd9080487f2f7ffde4a0
Rin-t <rin.t.4921@gmail.com>
2024-07-06 14:34:18 +0900

    Initial Commit

---
+++ QuizApp/QuizAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizAppApp.swift
+//  QuizApp
+//
+//  Created by Rin on 2024/07/06.
+//
+
+import SwiftUI
+
+@main
+struct QuizAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
