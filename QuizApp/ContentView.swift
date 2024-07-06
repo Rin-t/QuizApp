2b06a2ebdcad5c88e2ef084c1f09423ffaaf6afb
Rin-t <rin.t.4921@gmail.com>
2024-07-06 15:37:02 +0900

    create quizApp

--- QuizApp/ContentView.swift
+++ QuizApp/ContentView.swift
@@ -6,19 +6,163 @@
 //
 
 import SwiftUI
+import Observation
 
 struct ContentView: View {
+    @State var viewModel = ViewModel()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        switch viewModel.viewType {
+        case .finish:
+            Text("おわり！")
+                .font(.title)
+                .padding(.bottom, 24)
+            
+            Text("10問中\(viewModel.correctCount)問正解でした！")
+                .font(.title)
+                .padding(.bottom, 24)
+            
+        case .select:
+            VStack {
+                Text("この漢字の読みはなんでしょう？")
+                    .padding(.bottom, 24)
+                
+                Text(viewModel.currentQuiz.question)
+                    .font(.title)
+                    .padding(.bottom, 24)
+                
+                ChoicesView(firstChoice: viewModel.currentQuiz.choices[0],
+                            secondChoice: viewModel.currentQuiz.choices[1],
+                            thirdChoice: viewModel.currentQuiz.choices[2],
+                            fourthChoice: viewModel.currentQuiz.choices[3]) { title in
+                    viewModel.tappedAnswer(selectedAnswer: title)
+                }
+            }
+        case .result(let isCorrect):
+            VStack {
+                if isCorrect {
+                    Text("正解")
+                        .font(.title)
+                        .padding(.bottom, 24)
+                } else {
+                    Text("残念")
+                        .font(.title)
+                        .padding(.bottom, 24)
+                }
+                
+                Button {
+                    viewModel.tappedNextButton()
+                } label: {
+                    VStack {
+                        Text("つぎへ")
+                            .frame(width: 120)
+                            .padding()
+                            .background(Color.white)
+                            .cornerRadius(10)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(Color.black, lineWidth: 2)
+                            )
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
 }
+
+struct ChoicesView: View {
+    
+    let firstChoice: String
+    let secondChoice: String
+    let thirdChoice: String
+    let fourthChoice: String
+    
+    let action: (String) -> Void
+    
+    var body: some View {
+        VStack {
+            HStack {
+                choiceButton(choiceText: firstChoice) {
+                    action(firstChoice)
+                }
+                .padding(.trailing, 8)
+
+                choiceButton(choiceText: secondChoice) {
+                    action(secondChoice)
+                }
+            }
+            .padding(.bottom, 8)
+            
+            HStack {
+                choiceButton(choiceText: thirdChoice) {
+                    action(thirdChoice)
+                }
+                .padding(.trailing, 8)
+                
+                choiceButton(choiceText: fourthChoice) {
+                    action(fourthChoice)
+                }
+            }
+        }
+    }
+    
+    func choiceButton(choiceText: String, action: @escaping () -> Void) -> some View {
+        Button {
+            action()
+        } label: {
+            VStack {
+                Text(choiceText)
+                    .frame(width: 120)
+                    .padding()
+                    .background(Color.white)
+                    .cornerRadius(10)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(Color.black, lineWidth: 2)
+                    )
+            }
+        }
+    }
+}
+
+@Observable
+final class ViewModel {
+    
+    enum ViewType {
+        case select
+        case result(isCorrect: Bool)
+        case finish
+    }
+    
+    let quizes = Quiz.quizes
+    var currentQuizIndex = 0
+    var correctCount = 0
+    
+    var viewType: ViewType = .select
+    
+    var currentQuiz: Quiz {
+        quizes[currentQuizIndex]
+    }
+    
+    func tappedAnswer(selectedAnswer: String) {
+        let isCorrect = currentQuiz.isCorrect(selectedAnswer: selectedAnswer)
+        viewType = .result(isCorrect: isCorrect)
+        if isCorrect {
+            correctCount += 1
+        }
+    }
+    
+    func tappedNextButton() {
+        if currentQuizIndex == 9 {
+            viewType = .finish
+        } else {
+            currentQuizIndex += 1
+            viewType = .select
+        }
+    }
+}
+
--- QuizApp/Quiz.swift
+++ QuizApp/Quiz.swift
@@ -6,3 +6,66 @@
 //
 
 import Foundation
+
+struct Quiz {
+    let question: String
+    let choices: [String]
+    private let answerIndex: Int
+    
+    func isCorrect(selectedAnswer: String) -> Bool {
+        return choices[answerIndex] == selectedAnswer
+    }
+    
+    static let quizes: [Self] = [
+        Quiz(
+            question: "行脚",
+            choices: ["あんきゃ", "こうきゃ", "あんぎゃ", "こうぎゃ"],
+            answerIndex: 2
+        ),
+        Quiz(
+            question: "如実",
+            choices: ["にょじつ", "じょじつ", "にょしつ", "じょしつ"],
+            answerIndex: 0
+        ),
+        Quiz(
+            question: "英邁",
+            choices: ["えいまい", "えいび", "えいむ", "えいべい"],
+            answerIndex: 0
+        ),
+        Quiz(
+            question: "喬木",
+            choices: ["きょうぼく", "きょうもく", "きょうぎ", "きょうれい"],
+            answerIndex: 0
+        ),
+        Quiz(
+            question: "汎用",
+            choices: ["へんよう", "ばんよう", "はんよう", "ほうよう"],
+            answerIndex: 2
+        ),
+        Quiz(
+            question: "斬新",
+            choices: ["ざんしん", "せんしん", "しんせん", "しんざん"],
+            answerIndex: 0
+        ),
+        Quiz(
+            question: "勃興",
+            choices: ["ぼっこう", "ほっこう", "ぼつこう", "ほつこう"],
+            answerIndex: 0
+        ),
+        Quiz(
+            question: "頓挫",
+            choices: ["とんさ", "とんざ", "たんさ", "たんざ"],
+            answerIndex: 1
+        ),
+        Quiz(
+            question: "驚愕",
+            choices: ["きょうがく", "きょうかく", "けいがく", "けいかく"],
+            answerIndex: 0
+        ),
+        Quiz(
+            question: "燦爛",
+            choices: ["ざんらん", "さんらん", "ざんれん", "さんれん"],
+            answerIndex: 1
+        )
+    ]
+}
